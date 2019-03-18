fd2c3731f4e5fc112cc62b288ed2ae336e263b61
RuoYi <yzz_ivy@163.com>
2019-03-18 11:58:58 +0800

    添加新群号

--- src/main/java/com/ruoyi/framework/datasource/DynamicDataSource.java
+++ src/main/java/com/ruoyi/framework/datasource/DynamicDataSource.java
@@ -21,6 +21,6 @@ public class DynamicDataSource extends AbstractRoutingDataSource
     @Override
     protected Object determineCurrentLookupKey()
     {
-        return DynamicDataSourceContextHolder.getDateSoureType();
+        return DynamicDataSourceContextHolder.getDataSourceType();
     }
 }
