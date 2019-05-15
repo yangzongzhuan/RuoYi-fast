4f66437b4e47f62ae44efbe345b4b2a80430309d
RuoYi <yzz_ivy@163.com>
2019-05-15 10:45:56 +0800

    新增getPrincipalProperty方法，用于前端及其他模块获取当前用户信息

--- src/main/java/com/ruoyi/framework/web/service/PermissionService.java
+++ src/main/java/com/ruoyi/framework/web/service/PermissionService.java
@@ -1,6 +1,12 @@
 package com.ruoyi.framework.web.service;
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,6 +17,8 @@ import org.springframework.stereotype.Service;
 @Service("permission")
 public class PermissionService
 {
+    private static final Logger log = LoggerFactory.getLogger(PermissionService.class);
+
     public String hasPermi(String permission)
     {
         return isPermittedOperator(permission) ? "" : "hidden";
@@ -43,4 +51,35 @@ public class PermissionService
         return SecurityUtils.getSubject().hasRole(role);
     }
 
+    /**
+     * 返回用户属性值
+     *
+     * @param property 属性名称
+     * @return 用户属性值
+     */
+    public Object getPrincipalProperty(String property)
+    {
+        Subject subject = SecurityUtils.getSubject();
+        if (subject != null)
+        {
+            Object principal = subject.getPrincipal();
+            try
+            {
+                BeanInfo bi = Introspector.getBeanInfo(principal.getClass());
+                for (PropertyDescriptor pd : bi.getPropertyDescriptors())
+                {
+                    if (pd.getName().equals(property) == true)
+                    {
+                        return pd.getReadMethod().invoke(principal, (Object[]) null);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                log.error("Error reading property [{}] from principal of type [{}]", property,
+                        principal.getClass().getName());
+            }
+        }
+        return null;
+    }
 }
