478e739afaa35edd4890c509c38d56d8c46b4b58
RuoYi <yzz_ivy@163.com>
2019-05-30 13:23:50 +0800

    菜单管理新增打开方式

--- src/main/java/com/ruoyi/project/system/menu/domain/Menu.java
+++ src/main/java/com/ruoyi/project/system/menu/domain/Menu.java
@@ -14,37 +14,40 @@ import com.ruoyi.framework.web.domain.BaseEntity;
 public class Menu extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
-    
+
     /** 菜单ID */
     private Long menuId;
-    
+
     /** 菜单名称 */
     private String menuName;
-    
+
     /** 父菜单名称 */
     private String parentName;
-    
+
     /** 父菜单ID */
     private Long parentId;
-    
+
     /** 显示顺序 */
     private String orderNum;
-    
+
     /** 菜单URL */
     private String url;
-    
+
+    /** 打开方式：menuItem页签 menuBlank新窗口 */
+    private String target;
+
     /** 类型:0目录,1菜单,2按钮 */
     private String menuType;
-    
+
     /** 菜单状态:0显示,1隐藏 */
     private String visible;
-    
+
     /** 权限字符串 */
     private String perms;
-    
+
     /** 菜单图标 */
     private String icon;
-    
+
     /** 子菜单 */
     private List<Menu> children = new ArrayList<Menu>();
 
@@ -108,6 +111,16 @@ public class Menu extends BaseEntity
         this.url = url;
     }
 
+    public String getTarget()
+    {
+        return target;
+    }
+
+    public void setTarget(String target)
+    {
+        this.target = target;
+    }
+
     public String getMenuType()
     {
         return menuType;
@@ -166,6 +179,7 @@ public class Menu extends BaseEntity
             .append("parentId", getParentId())
             .append("orderNum", getOrderNum())
             .append("url", getUrl())
+            .append("target", getTarget())
             .append("menuType", getMenuType())
             .append("visible", getVisible())
             .append("perms", getPerms())
