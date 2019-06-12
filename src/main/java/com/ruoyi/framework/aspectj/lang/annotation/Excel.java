d0d7887418c4c1e8e162113a376d55f7749c05a0
RuoYi <yzz_ivy@163.com>
2019-06-12 16:43:02 +0800

    Excel导出子对象支持多个字段

--- src/main/java/com/ruoyi/framework/aspectj/lang/annotation/Excel.java
+++ src/main/java/com/ruoyi/framework/aspectj/lang/annotation/Excel.java
@@ -1,7 +1,6 @@
 package com.ruoyi.framework.aspectj.lang.annotation;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -13,7 +12,6 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-@Repeatable(Excels.class)
 public @interface Excel
 {
     /**
