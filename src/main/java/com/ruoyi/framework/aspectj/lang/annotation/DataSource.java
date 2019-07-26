613fe28374161a980803d43a8c0e5955c174a44c
RuoYi <yzz_ivy@163.com>
2019-07-26 20:22:33 +0800

    多数据源支持类注解（允许继承父类的注解）

--- src/main/java/com/ruoyi/framework/aspectj/lang/annotation/DataSource.java
+++ src/main/java/com/ruoyi/framework/aspectj/lang/annotation/DataSource.java
@@ -1,10 +1,11 @@
 package com.ruoyi.framework.aspectj.lang.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
 import com.ruoyi.framework.aspectj.lang.enums.DataSourceType;
 
 /**
@@ -12,8 +13,10 @@ import com.ruoyi.framework.aspectj.lang.enums.DataSourceType;
  * 
  * @author ruoyi
  */
-@Target(ElementType.METHOD)
+@Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
 public @interface DataSource
 {
     /**
