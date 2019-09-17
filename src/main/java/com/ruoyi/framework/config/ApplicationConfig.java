2971a6499ea3bc37ca392f0d067c715dd8a63c9c
RuoYi <yzz_ivy@163.com>
2019-09-17 20:06:11 +0800

    支持通配符扫描任意多个包，防止类名重复异常

--- src/main/java/com/ruoyi/framework/config/ApplicationConfig.java
+++ src/main/java/com/ruoyi/framework/config/ApplicationConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 // 表示通过aop框架暴露该代理对象,AopContext能够访问
 @EnableAspectJAutoProxy(exposeProxy = true)
 // 指定要扫描的Mapper类的包的路径
-@MapperScan("com.ruoyi.project.*.*.mapper")
+@MapperScan("com.ruoyi.project.**.mapper")
 public class ApplicationConfig
 {
 
