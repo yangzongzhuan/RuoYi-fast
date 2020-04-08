1a98b5fa3e382f8419552dab60913e322dae0af6
RuoYi <yzz_ivy@163.com>
2020-04-08 11:08:38 +0800

    删除无用注解

--- src/main/java/com/ruoyi/framework/interceptor/annotation/RepeatSubmit.java
+++ src/main/java/com/ruoyi/framework/interceptor/annotation/RepeatSubmit.java
@@ -2,7 +2,6 @@ package com.ruoyi.framework.interceptor.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -13,7 +12,6 @@ import java.lang.annotation.Target;
  * @author ruoyi
  *
  */
-@Inherited
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
