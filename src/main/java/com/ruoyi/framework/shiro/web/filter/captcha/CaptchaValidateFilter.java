a8ce671b31208f5380b4fcddf53337b7f6fddfc6
RuoYi <yzz_ivy@163.com>
2020-06-28 16:30:54 +0800

    验证码清除，防止多次使用。

--- src/main/java/com/ruoyi/framework/shiro/web/filter/captcha/CaptchaValidateFilter.java
+++ src/main/java/com/ruoyi/framework/shiro/web/filter/captcha/CaptchaValidateFilter.java
@@ -61,6 +61,8 @@ public class CaptchaValidateFilter extends AccessControlFilter
     {
         Object obj = ShiroUtils.getSession().getAttribute(Constants.KAPTCHA_SESSION_KEY);
         String code = String.valueOf(obj != null ? obj : "");
+        // 验证码清除，防止多次使用。
+        request.getSession().removeAttribute(Constants.KAPTCHA_SESSION_KEY);
         if (StringUtils.isEmpty(validateCode) || !validateCode.equalsIgnoreCase(code))
         {
             return false;
