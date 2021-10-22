120d7f2105a266fa5465bdc32605e0118c4d17f2
RuoYi <yzz_ivy@163.com>
2021-10-22 16:02:13 +0800

    update ry.sh.

--- ry.sh
+++ ry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ./ry.sh start 启动 stop 停止 restart 重启 status 状态
-AppName=target/ruoyi-vue.jar
+AppName=target/ruoyi.jar
 
 # JVM参数
 JVM_OPTS="-Dname=$AppName  -Duser.timezone=Asia/Shanghai -Xms512m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
