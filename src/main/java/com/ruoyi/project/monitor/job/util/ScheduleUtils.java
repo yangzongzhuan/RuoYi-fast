b1214770d508ee29f68c0f2640cce372bc5ee958
RuoYi <yzz_ivy@163.com>
2022-04-16 20:57:26 +0800

    定时任务bean也检查包名是否为白名单配置

--- src/main/java/com/ruoyi/project/monitor/job/util/ScheduleUtils.java
+++ src/main/java/com/ruoyi/project/monitor/job/util/ScheduleUtils.java
@@ -14,6 +14,7 @@ import com.ruoyi.common.constant.ScheduleConstants;
 import com.ruoyi.common.exception.job.TaskException;
 import com.ruoyi.common.exception.job.TaskException.Code;
 import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.common.utils.spring.SpringUtils;
 import com.ruoyi.project.monitor.job.domain.Job;
 
 /**
@@ -126,6 +127,7 @@ public class ScheduleUtils
         {
             return StringUtils.containsAnyIgnoreCase(invokeTarget, Constants.JOB_WHITELIST_STR);
         }
-        return true;
+        Object obj = SpringUtils.getBean(StringUtils.split(invokeTarget, ".")[0]);
+        return StringUtils.containsAnyIgnoreCase(obj.getClass().getPackage().getName(), Constants.JOB_WHITELIST_STR);
     }
 }
