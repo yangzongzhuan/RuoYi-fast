556ee17a89dae3890241d9f90ec999ec140b3ebf
RuoYi <yzz_ivy@163.com>
2022-09-02 09:50:52 +0800

    AjaxResult错误消息结果类型的判断

--- src/main/java/com/ruoyi/framework/web/domain/AjaxResult.java
+++ src/main/java/com/ruoyi/framework/web/domain/AjaxResult.java
@@ -198,7 +198,7 @@ public class AjaxResult extends HashMap<String, Object>
      */
     public boolean isError()
     {
-        return Objects.equals(Type.SUCCESS, this.get(CODE_TAG));
+        return Objects.equals(Type.ERROR.value, this.get(CODE_TAG));
     }
 
     /**
@@ -214,5 +214,4 @@ public class AjaxResult extends HashMap<String, Object>
         super.put(key, value);
         return this;
     }
-
 }
