87c225b17c5f2f1f993d0578a17c51d9e5bbb2a0
RuoYi <yzz_ivy@163.com>
2022-12-03 11:16:21 +0800

    定时任务违规的字符

--- src/main/java/com/ruoyi/common/constant/Constants.java
+++ src/main/java/com/ruoyi/common/constant/Constants.java
@@ -111,5 +111,5 @@ public class Constants
      * 定时任务违规的字符
      */
     public static final String[] JOB_ERROR_STR = { "java.net.URL", "javax.naming.InitialContext", "org.yaml.snakeyaml",
-            "org.springframework", "com.ruoyi.common.utils.file" };
+            "org.springframework", "com.ruoyi.common.utils.file", "com.ruoyi.framework.config" };
 }
