321aa09a4e7daaf3eecb77fa3bf5945c785e6c2a
RuoYi <yzz_ivy@163.com>
2024-01-05 14:28:26 +0800

    优化注册成功弹框图标没有显示问题

--- src/main/resources/static/ruoyi/register.js
+++ src/main/resources/static/ruoyi/register.js
@@ -5,6 +5,7 @@ $(function() {
         var url = ctx + "captcha/captchaImage?type=" + captchaType + "&s=" + Math.random();
         $(".imgcode").attr("src", url);
     });
+    layer.config({ extend: 'moon/style.css', skin: 'layer-ext-moon' });
 });
 
 $.validator.setDefaults({
