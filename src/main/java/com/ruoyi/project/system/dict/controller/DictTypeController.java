69f05c0dbd51f84591bdadad20f3fcf95ef3b1e9
RuoYi <yzz_ivy@163.com>
2025-12-02 11:08:36 +0800

    优化代码

--- src/main/java/com/ruoyi/project/system/dict/controller/DictTypeController.java
+++ src/main/java/com/ruoyi/project/system/dict/controller/DictTypeController.java
@@ -166,7 +166,7 @@ public class DictTypeController extends BaseController
      * 选择字典树
      */
     @GetMapping("/selectDictTree/{columnId}/{dictType}")
-    public String selectDeptTree(@PathVariable("columnId") Long columnId, @PathVariable("dictType") String dictType, ModelMap mmap)
+    public String selectDictTree(@PathVariable("columnId") Long columnId, @PathVariable("dictType") String dictType, ModelMap mmap)
     {
         mmap.put("columnId", columnId);
         mmap.put("dict", dictTypeService.selectDictTypeByType(dictType));
